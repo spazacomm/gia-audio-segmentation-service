4e18d892d74dfe85cb13cc52fdf5ee312cf3025b
Jack Chumo <jacqchumo@gmail.com>
2025-12-19 13:45:54 +0300

    chnages

--- batch-fingerprint-service/job.py
+++ batch-fingerprint-service/job.py
@@ -69,8 +69,8 @@ def retry_on_failure(max_attempts=MAX_RETRIES, delay=RETRY_DELAY):
 class AudioFingerprintJob:
     def __init__(self):
         self.supabase: Client = create_client(
-            os.environ["SUPABASE_URL"],
-            os.environ["SUPABASE_SERVICE_KEY"],
+            "https://nclmnuzdgmcuobduhteo.supabase.co",
+            "sb_secret_SaWrmejnvumwHLMgVqyExA_GVK3N1gg",
         )
         self.storage = storage.Client()
         self.library_bucket = self.storage.bucket(LIBRARY_BUCKET)
